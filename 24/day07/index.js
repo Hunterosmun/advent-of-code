5ca5684fafb3b28ed9f4ed21852af46b50a56a1e
Hunterosmun <Hunter@Osmun.net>
2024-12-12 08:31:05 -0700

    2024 day7 part 2

--- 24/day07/index.js
+++ 24/day07/index.js
@@ -48,3 +48,39 @@ function getTotals(usedParts, parts) {
 assert.strictEqual(main(ex1), 3749)
 assert.strictEqual(main(santasList), 3245122495150)
 
+function partTwo(input) {
+  return input
+    .trim()
+    .split('\n')
+    .reduce((acc, line) => {
+      let [total, parts] = line.split(':')
+      total = Number(total)
+      parts = parts.trim().split(' ').map(Number)
+
+      const totals = getTotalsPartTwo([], parts)
+      let validTotal = false
+      totals.forEach((t) => {
+        if (t === total) validTotal = true
+      })
+
+      if (validTotal) return acc + total
+      else return acc
+    }, 0)
+}
+
+function getTotalsPartTwo(usedParts, parts) {
+  if (!parts.length) return usedParts
+  if (!usedParts.length) {
+    usedParts.push(parts[0])
+    return getTotalsPartTwo(usedParts, parts.slice(1))
+  }
+  return usedParts.flatMap((used) => {
+    return getTotalsPartTwo(
+      [used + parts[0], used * parts[0], Number(`${used}` + parts[0])],
+      parts.slice(1)
+    )
+  })
+}
+
+assert.strictEqual(partTwo(ex1), 11387)
+assert.strictEqual(partTwo(santasList), 105517128211543)
